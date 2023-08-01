3e8fcd6d16cc7e091b8a5b41415775d2dc7a7059
samBamkole <olagoroyesamuel7@gmail.com>
2023-08-01 13:57:13 +0100

    testing 2

--- contentful.js
+++ contentful.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const contentful = require("contentful");
 
 const client = contentful.createClient({
-    space: process.env.CONTENT_SPACE_ID,
-    accessToken: process.env.CONTENT_ACCESS_TOKEN,
+    space: "h4ger8mte7g9",
+    accessToken: "oxw7zdNi7iOnlKm84iXdAKxVu8BXt2a-8V_d97pfVJQ",
 });
 
 module.exports = client;
