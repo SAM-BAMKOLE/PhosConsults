fc589e3581407f48ae3a197c5e126f3a5b094d7c
samBamkole <olagoroyesamuel7@gmail.com>
2023-08-12 04:29:19 +0100

    fixed php file

--- src/message.php
+++ src/message.php
@@ -9,8 +9,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if(!empty($firstname) && !empty($lastname) && !empty($email) && !empty($phone) && !empty($message)) {
         if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $receiver = "samuelolagoroye81@gmail.com";
-            // $subject = "From: $firstname $lastname <$email>";
+            $receiver = "samuelolagoroye81@gmai.com";
+            $subject = "$options";
             $body = "Name: $firstname $lastname\nEmail: $email\nPhone: $phone\nInquiry: $options\nMessage: $message";
             $sender = "From $email";
             if(mail($receiver, $subject, $body, $sender)) {
